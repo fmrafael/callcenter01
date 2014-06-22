86c1b57cb2df20aecdb6d22d10f6de8ef085a7e0
rafael <rafaelfernandesmarcondes@gmail.com>
2014-06-22 16:47:18 -0300

    setup

--- sms/admin.py
+++ sms/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from sms.models import Frase
+
+admin.site.register(Frase)
 
 # Register your models here.
--- sms/models.py
+++ sms/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Frase(models.Model):
+    
+    frase_dia = models.CharField(max_length=400)
+    pub_date = models.DateTimeField('date published')
+    author = models.CharField(max_length=200)
+    def __unicode__(self):
+        return self.frase_dia
+
+    
+
+
